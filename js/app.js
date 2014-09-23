10db53a0681fa1c322eaa0c3a3fffe1cf192edec
Travis Tidwell <codertravist@gmail.com>
2014-09-22 21:50:06 -0500

    Adding a video modal.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 /** Your custom JavaScript goes here. **/
+(function($) {
+  $(function() {
+
+    // Handle the video modal.
+    $('#video-modal').on('shown.bs.modal', function(event) {
+
+      // Load the video.
+      $('.modal-body .embed-responsive', this).append($('#video-template').html());
+    }).on('hidden.bs.modal', function(event) {
+
+      // Remove the video
+      $('.modal-body .embed-responsive iframe', this).remove();
+    });
+  });
+})(jQuery);
