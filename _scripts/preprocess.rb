56aadae68ca5297d480e986d78a57f922fe7d414
Peter Binkley <peter.binkley@ualberta.ca>
2016-05-07 08:14:32 -0600

    Add _scripts/preprocess.rb

---
+++ _scripts/preprocess.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# modify markdown before rendering pdf
+# this is a skeleton script that does nothing; it should
+# be modified for the needs of a given project
+ARGF.each_with_index do |line, idx|
+	print line
+end
